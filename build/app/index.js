bcadaf4b63f7dae24f8e00bcbfd7535a48ce8373
Souvik <souvikroy1999ab@gmail.com>
2023-08-16 22:36:06 +0530

    added authentication

--- build/app/index.js
+++ build/app/index.js
@@ -19,6 +19,7 @@ const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const express_1 = __importDefault(require("express"));
 const user_1 = require("./user");
+const jwt_1 = __importDefault(require("../services/jwt"));
 function initServer() {
     return __awaiter(this, void 0, void 0, function* () {
         const app = (0, express_1.default)();
@@ -36,7 +37,15 @@ function initServer() {
             },
         });
         yield graphqlServer.start();
-        app.use('/graphql', (0, express4_1.expressMiddleware)(graphqlServer));
+        app.use('/graphql', (0, express4_1.expressMiddleware)(graphqlServer, {
+            context: ({ req, res }) => __awaiter(this, void 0, void 0, function* () {
+                return {
+                    user: req.headers.authorization
+                        ? jwt_1.default.decodeToken(req.headers.authorization.split('Bearer ')[1])
+                        : undefined,
+                };
+            }),
+        }));
         return app;
     });
 }
--- src/app/index.ts
+++ src/app/index.ts
@@ -5,6 +5,8 @@ import bodyParser from 'body-parser';
 import express from 'express';
 
 import { User } from './user';
+import { GraphqlContext } from '../interfaces';
+import JWTService from '../services/jwt';
 
 export async function initServer() {
   const app = express();
@@ -12,7 +14,7 @@ export async function initServer() {
   app.use(bodyParser.json());
   app.use(cors());
 
-  const graphqlServer = new ApolloServer({
+  const graphqlServer = new ApolloServer<GraphqlContext>({
     typeDefs: `
     ${User.types}
         type Query{
@@ -28,7 +30,20 @@ export async function initServer() {
 
   await graphqlServer.start();
 
-  app.use('/graphql', expressMiddleware(graphqlServer));
+  app.use(
+    '/graphql',
+    expressMiddleware(graphqlServer, {
+      context: async ({ req, res }) => {
+        return {
+          user: req.headers.authorization
+            ? JWTService.decodeToken(
+                req.headers.authorization.split('Bearer ')[1]
+              )
+            : undefined,
+        };
+      },
+    })
+  );
 
   return app;
 }
